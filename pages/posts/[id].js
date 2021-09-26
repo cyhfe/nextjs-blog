08f46242f0ae429c0de46c7f3364ef63f776ec3f
chenyuhao <cccchenyuhao@gmail.com>
2021-09-26 22:01:57 +0800

    deploy

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,25 +1,39 @@
-import Layout from "../../components/layout";
-import { useRouter } from "next/router";
-import { getAllPostIds } from "../../lib/posts";
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
-export function Post() {
-  return <Layout>...</Layout>;
-}
-
-export default function FirstPost() {
-  const router = useRouter();
-  const { id } = router.query;
+export default function Post({ postData }) {
   return (
     <Layout>
-      <h1>{id} Post</h1>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-  );
+  )
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = getAllPostIds()
   return {
     paths,
-    fallback: false,
-  };
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
