86d757805c51c6120ee590386bd2c7ef7c6bfffa
chenyuhao <cccchenyuhao@gmail.com>
2021-09-26 22:06:55 +0800

    change name & description

--- components/layout.js
+++ components/layout.js
@@ -4,18 +4,14 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'chenyuhao'
+export const siteTitle = 'next-blog'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
