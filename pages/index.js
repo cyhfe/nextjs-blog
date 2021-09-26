8010f9079d0c594e03b10a9b3f61fe5ee770103b
chenyuhao <cccchenyuhao@gmail.com>
2021-09-26 22:22:27 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Dont's Repeat Your self</p>
+        <p>Dont's Repeat Yourself</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
