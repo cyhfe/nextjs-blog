29ce773c87486251d848572bc2a5d79c1a6569f3
chenyuhao <cccchenyuhao@gmail.com>
2022-01-06 21:17:53 +0800

    update

--- posts/index.js
+++ posts/index.js
@@ -1,48 +1,8 @@
-class Pubsub {
-  constructor() {
-    this.eventBus = {};
-  }
-
-  subscribe(event, fn) {
-    if (!this.eventBus[event]) {
-      this.eventBus[event] = [];
+function foo(arg) {
+    function bar(){
+        console.log(arguments)
     }
-    this.eventBus[event].push(fn);
-  }
-
-  unsubscribe(event, fn) {
-    if (this.eventBus[event]) return false;
-    this.eventBus[event].filter((f) => f !== fn);
-  }
-
-  publish(event, data) {
-    if (!this.eventBus[event]) return false;
-    this.eventBus[event].forEach((fn) => fn(data));
-  }
+    bar(arg)
 }
 
-const pubsub = new Pubsub();
-
-const eat = function () {
-  console.log("I am eating");
-};
-
-const drink = function () {
-  console.log("I am drinking");
-};
-const running = function () {
-  console.log("I am running");
-};
-
-pubsub.subscribe("dinner", eat);
-pubsub.subscribe("dinner", drink);
-pubsub.subscribe("sports", running);
-
-pubsub.publish("dinner");
-// should log
-// I am drinking
-// I am eating
-
-pubsub.publish("sports");
-// should log
-// I am running
+foo(1, 2, 3)
