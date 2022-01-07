c461c6429a01767142d9b6d8e5e3f9d106e96309
chenyuhao <cccchenyuhao@gmail.com>
2022-01-07 20:23:53 +0800

    update

--- posts/index.js
+++ posts/index.js
@@ -208,9 +208,69 @@
 // defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument.
 // Additional arguments needed by the passed-in function will need to be passed into the returned function.
 
-function defineFirstArg(func, arg) {}
+// function defineFirstArg(func, arg) {
+//   return function (...rest) {
+//     console.log(func(arg, ...rest));
+//   };
+// }
 
-// /*** Uncomment these to check your work! ***/
-// const subtract = function(big, small) { return big - small; };
+// // /*** Uncomment these to check your work! ***/
+// const subtract = function (big, small) {
+//   return big - small;
+// };
 // const subFrom20 = defineFirstArg(subtract, 20);
 // console.log(subFrom20(5)); // => should log 15
+
+// Challenge 11
+// Create a function dateStamp that accepts a function and returns a function.
+// The returned function will accept however many arguments the passed-in function accepts,
+// and return an object with a date key that contains a timestamp with the time of invocation,
+// and an output key that contains the result from invoking the passed-in function.
+// HINT: You may need to research how to access information on Date objects.
+
+// function dateStamp(func) {
+//   return function (...arg) {
+//     const output = func(...arg);
+//     const date = new Date().toLocaleString();
+//     const ret = {
+//       date,
+//       output,
+//     };
+//     console.log(ret);
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// const stampedMultBy2 = dateStamp((n) => n * 2);
+// console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+// console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+
+// Challenge 12
+// Create a function censor that accepts no arguments.
+// censor will return a function that will accept either two strings, or one string.
+// When two strings are given, the returned function will hold onto the two strings as a pair, for future use.
+// When one string is given, the returned function will return the same string,
+// except all instances of first strings (of saved pairs) will be replaced with their corresponding second strings (of those saved pairs).
+
+// function censor() {
+//   const pair = [];
+//   return function (...rest) {
+//     if (rest.length === 1) {
+//       let ret = null;
+//       pair.forEach((p) => {
+//         ret = rest[0].replace(new RegExp(p[0]), p[1]);
+//       });
+//       return ret;
+//     }
+
+//     if (rest.length === 2) {
+//       pair.push(rest);
+//     }
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// const changeScene = censor();
+// changeScene("dogs", "cats");
+// changeScene("quick", "slow");
+// console.log(changeScene("The quick, brown fox jumps over the lazy dogs.")); // => should log 'The slow, brown fox jumps over the lazy cats.'
