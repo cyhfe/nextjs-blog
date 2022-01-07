d26493673a043c6bc23bbc242461de7dfe707ff6
chenyuhao <cccchenyuhao@gmail.com>
2022-01-07 19:19:28 +0800

    update

--- posts/index.js
+++ posts/index.js
@@ -150,3 +150,67 @@
 // rollCaller(); // => should log 'Juan'
 // rollCaller(); // => should log 'Ruth'
 // rollCaller(); // => should log 'Everyone accounted for'
+
+// Challenge 8
+// Create a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password).
+// saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument.
+// When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values.
+
+// function saveOutput(func, magicWord) {
+//   const map = {};
+//   return function (arg) {
+//     if (arg === magicWord) {
+//       console.log(map);
+//       return;
+//     }
+
+//     const ret = func(arg);
+//     map[arg] = ret;
+//     console.log(ret);
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// const multiplyBy2 = function (num) {
+//   return num * 2;
+// };
+// const multBy2AndLog = saveOutput(multiplyBy2, "boo");
+// console.log(multBy2AndLog(2)); // => should log 4
+// console.log(multBy2AndLog(9)); // => should log 18
+// console.log(multBy2AndLog("boo")); // => should log { 2: 4, 9: 18 }
+
+// CHALLENGE 9
+// Create a function cycleIterator that accepts an array, and returns a function.
+// The returned function will accept zero arguments. When first invoked, the returned function will return the first element of the array.
+// When invoked a second time, the returned function will return the second element of the array, and so forth.
+// After returning the last element of the array, the next invocation will return the first element of the array again, and continue on with the second after that, and so forth.
+// function cycleIterator(array) {
+//   let index = 0;
+//   return function () {
+//     if (index >= array.length) {
+//       index = 0;
+//     }
+//     console.log(array[index]);
+//     index++;
+//   };
+// }
+
+// const threeDayWeekend = ["Fri", "Sat", "Sun"];
+// const getDay = cycleIterator(threeDayWeekend);
+// console.log(getDay()); // => should log 'Fri'
+// console.log(getDay()); // => should log 'Sat'
+// console.log(getDay()); // => should log 'Sun'
+// console.log(getDay()); // => should log 'Fri'
+
+// Challenge 10
+// Create a function defineFirstArg that accepts a function and an argument.
+// Also, the function being passed in will accept at least one argument.
+// defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument.
+// Additional arguments needed by the passed-in function will need to be passed into the returned function.
+
+function defineFirstArg(func, arg) {}
+
+// /*** Uncomment these to check your work! ***/
+// const subtract = function(big, small) { return big - small; };
+// const subFrom20 = defineFirstArg(subtract, 20);
+// console.log(subFrom20(5)); // => should log 15
