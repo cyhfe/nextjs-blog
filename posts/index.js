b691a5a076f855e9014a8815cd3af813a43a58c9
chenyuhao <cccchenyuhao@gmail.com>
2022-01-07 21:46:40 +0800

    update

--- posts/index.js
+++ posts/index.js
@@ -274,3 +274,202 @@
 // changeScene("dogs", "cats");
 // changeScene("quick", "slow");
 // console.log(changeScene("The quick, brown fox jumps over the lazy dogs.")); // => should log 'The slow, brown fox jumps over the lazy cats.'
+
+// Challenge 13
+// There's no such thing as private properties on a JavaScript object!
+// But, maybe there are? Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods.
+// getSecret() which returns the secret setSecret() which sets the secret
+
+// function createSecretHolder(secret) {
+//   return {
+//     getSecret() {
+//       return secret;
+//     },
+//     setSecret(x) {
+//       secret = x;
+//     },
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// obj = createSecretHolder(5);
+// obj.getSecret(); // => returns 5
+// obj.setSecret(2);
+// obj.getSecret(); // => returns 2
+
+// Challenge 14
+// Write a function, callTimes, that returns a new function. The new function should return the number of times it’s been called.
+
+// function callTimes() {
+//   let called = 0;
+//   return function () {
+//     console.log(++called);
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// let myNewFunc1 = callTimes();
+// let myNewFunc2 = callTimes();
+// myNewFunc1(); // => 1
+// myNewFunc1(); // => 2
+// myNewFunc2(); // => 1
+// myNewFunc2(); // => 2
+
+// Challenge 15
+// Create a function russianRoulette that accepts a number (let us call it n),
+// and returns a function. The returned function will take no arguments,
+// and will return the string 'click' the first n - 1 number of times it is invoked.
+// On the very next invocation (the nth invocation), the returned function will return the string 'bang'.
+// On every invocation after that, the returned function returns the string 'reload to play again'.
+
+// function russianRoulette(num) {
+//   let called = 0;
+//   return function () {
+//     called++;
+//     if (called < num) {
+//       console.log("click");
+//     } else if (called === num) {
+//       console.log("bang");
+//     } else {
+//       console.log("reload to play again");
+//     }
+//   };
+// }
+
+// const play = russianRoulette(3);
+// console.log(play()); // => should log 'click'
+// console.log(play()); // => should log 'click'
+// console.log(play()); // => should log 'bang'
+// console.log(play()); // => should log 'reload to play again'
+// console.log(play()); // => should log 'reload to play again'
+
+// Challenge 16
+// Create a function average that accepts no arguments, and returns a function (that will accept either a number as its lone argument, or no arguments at all).
+// When the returned function is invoked with a number,
+// the output should be average of all the numbers have ever been passed into that function (duplicate numbers count just like any other number).
+// When the returned function is invoked with no arguments, the current average is outputted.
+// If the returned function is invoked with no arguments before any numbers are passed in, then it should return 0.
+
+// function average() {
+//   let cache = [];
+//   let ret = 0;
+//   return function (...arg) {
+//     if (arg.length === 0) {
+//       console.log(ret);
+//     } else if (arg.length === 1) {
+//       cache.push(arg[0]);
+//       let sum = cache.reduce((accu, curr) => {
+//         return accu + curr;
+//       });
+//       ret = sum / cache.length;
+//       console.log(ret);
+//     }
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// const avgSoFar = average();
+// console.log(avgSoFar()); // => should log 0
+// console.log(avgSoFar(4)); // => should log 4
+// console.log(avgSoFar(8)); // => should log 6
+// console.log(avgSoFar()); // => should log 6
+// console.log(avgSoFar(12)); // => should log 8
+// console.log(avgSoFar()); // => should log 8
+
+// Challenge 17
+// Create a function makeFuncTester that accepts an array (of two-element sub-arrays), and returns a function (that will accept a callback).
+// The returned function should return true if the first elements (of each sub-array) being passed into the callback all yield the corresponding second elements (of the same sub-array).
+// Otherwise, the returned function should return false.
+
+// function makeFuncTester(arrOfTests) {
+//   return function (cb) {
+//     const ret = arrOfTests.every((arr) => {
+//       return cb(arr[0]) === arr[1];
+//     });
+//     if (ret) return true;
+//     return false;
+//   };
+// }
+
+// // /*** Uncomment these to check your work! ***/
+// const capLastTestCases = [];
+// capLastTestCases.push(["hello", "hellO"]);
+// capLastTestCases.push(["goodbye", "goodbyE"]);
+// capLastTestCases.push(["howdy", "howdY"]);
+// const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+// const capLastAttempt1 = (str) => str.toUpperCase();
+// const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
+// console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+// console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
+
+// Challenge 18
+// Create a function makeHistory that accepts a number (which will serve as a limit), and returns a function (that will accept a string).
+// The returned function will save a history of the most recent "limit" number of strings passed into the returned function (one per invocation only).
+// Every time a string is passed into the function, the function should return that same string with the word 'done' after it (separated by a space).
+// However, if the string 'undo' is passed into the function,
+// then the function should delete the last action saved in the history, and return that deleted string with the word 'undone' after (separated by a space).
+// If 'undo' is passed into the function and the function's history is empty, then the function should return the string 'nothing to undo'.
+
+// function makeHistory(limit) {
+//   const history = [];
+//   return function (string) {
+//     if (string === "undo") {
+//       if (history.length === 0) {
+//         console.log("nothing to undo");
+//         return;
+//       }
+//       console.log(history[history.length - 1] + "undone");
+//       history.pop();
+//     } else {
+//       history.push(string);
+//       if (history.length > limit) history.pop();
+//       console.log(string + "done");
+//     }
+//   };
+// }
+
+// const myActions = makeHistory(2);
+// console.log(myActions("jump")); // => should log 'jump done'
+// console.log(myActions("undo")); // => should log 'jump undone'
+// console.log(myActions("walk")); // => should log 'walk done'
+// console.log(myActions("code")); // => should log 'code done'
+// console.log(myActions("pose")); // => should log 'pose done'
+// console.log(myActions("undo")); // => should log 'pose undone'
+// console.log(myActions("undo")); // => should log 'code undone'
+// console.log(myActions("undo")); // => should log 'nothing to undo'
+
+function blackjack(array) {}
+
+// /*** Uncomment these to check your work! ***/
+
+// /*** DEALER ***/
+// const deal = blackjack([2, 6, 1, 7, 11, 4, 6, 3, 9, 8, 9, 3, 10, 4, 5, 3, 7, 4, 9, 6, 10, 11]);
+
+// /*** PLAYER 1 ***/
+// const i_like_to_live_dangerously = deal(4, 5);
+// console.log(i_like_to_live_dangerously()); // => should log 9
+// console.log(i_like_to_live_dangerously()); // => should log 11
+// console.log(i_like_to_live_dangerously()); // => should log 17
+// console.log(i_like_to_live_dangerously()); // => should log 18
+// console.log(i_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+// console.log(i_like_to_live_dangerously()); // => should log 'you are done!'
+
+// /*** BELOW LINES ARE FOR THE BONUS ***/
+
+// /*** PLAYER 2 ***/
+// const i_TOO_like_to_live_dangerously = deal(2, 2);
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 4
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 15
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 19
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_TOO_like_to_live_dangerously()); // => should log 'you are done!
+
+// /*** PLAYER 3 ***/
+// const i_ALSO_like_to_live_dangerously = deal(3, 7);
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 10
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 13
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'bust'
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
+// console.log(i_ALSO_like_to_live_dangerously()); // => should log 'you are done!
