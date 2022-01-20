4afe1a0d3c22dd6bf1cb2fe444a3655ecee9ddf5
chenyuhao <cccchenyuhao@gmail.com>
2022-01-21 01:23:22 +0800

    ga

--- pages/_app.js
+++ pages/_app.js
@@ -9,21 +9,42 @@ import "@fontsource/roboto/700.css"
 
 import Head from "next/head"
 
+import * as ga from "../lib/ga"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
 const siteTitle = "blog - nextjs"
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Component {...pageProps}>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
         <meta
-          property='og:image'
+          property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name='og:title' content={siteTitle} />
-        <meta name='twitter:card' content='summary_large_image' />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
     </Component>
   )
